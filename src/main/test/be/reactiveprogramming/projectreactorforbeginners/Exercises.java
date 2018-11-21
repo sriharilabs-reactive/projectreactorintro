98669796769d83da82149b1fdba7bdcc16ec1b62
sterkje <jeroen.sterken@faros.be>
2018-11-21 14:18:26 +0100

    Added 3 exercises (backpressure, multiple subscribers)

--- src/main/test/be/reactiveprogramming/projectreactorforbeginners/Exercises.java
+++ src/main/test/be/reactiveprogramming/projectreactorforbeginners/Exercises.java
@@ -1,10 +1,15 @@
 package be.reactiveprogramming.projectreactorforbeginners;
 
+import java.time.Duration;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 import org.junit.Test;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 
 public class Exercises {
@@ -71,7 +76,7 @@ public class Exercises {
    * We expect to see the numbers "4, 8, 12, 16, 20" printed on the command line.
    */
 
-  private Consumer<Integer> commandLinePrinter = o -> System.out.println(o);
+  private Consumer<Object> commandLinePrinter = o -> System.out.println(o);
 
   @Test
   public void publisherTest() {
@@ -149,4 +154,156 @@ public class Exercises {
     StepVerifier.create(lettersAndNumbers).expectNext("a1", "b2", "c3", "d4", "e5").verifyComplete();
   }
 
+  /**
+   * TODO 06 Nonblocking backpressure
+   *
+   * Look at the code, you'll see a (fast) publisher/flux and a (slow) subscriber/processor.
+   * Run the test and notice that both the producer & subscriber run synchronously: emitting 1 element & processing it before the next element is emitted.
+   * In this case the slowest component (= the subscriber) in the chain dictates the speed of the emitter/publisher.
+   *
+   * This is not what we want. We don't want the subscriber dictating the publishers speed and on the other hand the publisher should
+   * not dictate the speed of how fast the subscriber can process the data. Both the Publisher/subscriber should be able to transmit/process data
+   * at their own pace without affecting each other. Let's do this!
+   *
+   * Uncomment the publishOn method and re-run the test. Now you will see that the publisher and subscriber will be working on their own pace.
+   * The publisher (emitting data) will finish earlier than the subscriber. So they work async now because 'publishOn()' influences the threading
+   * context where the rest of the operators in the chain below it will execute, up to a new occurrence of publishOn.
+   *
+   * You might have noticed that despite the faster speed of the publisher, all elements are eventually being send down to the subscriber/processor.
+   * That is because, by default, the publisher will buffer elements in case of a slow subscriber, emitting the elements when the subscriber is ready for the next.
+   * Let's change this to a drop-strategy by using the correct create method on Flux. Now run the test again and notice that some elements will not be processed
+   * because they are beging dropped by the publisher.
+   */
+  @Test
+  public void backPressure() {
+    Flux<Integer> numbers = Flux.create(emitter -> emit(emitter), FluxSink.OverflowStrategy.DROP);
+
+    numbers
+           .publishOn(Schedulers.parallel(), 1)
+           .map(e -> e * 1)
+           .subscribe(e -> slowProcessor(e),
+                      err -> System.out.println(err),
+                      () -> System.out.println("DONE"));
+
+    sleep(10000);
+  }
+
+  private void slowProcessor(Integer i) {
+    System.out.println("   >> processing: "+i);
+    sleep(1000);
+  }
+
+  private void emit(FluxSink<Integer> emitter) {
+    int count = 0;
+
+    while (count < 10) {
+      count++;
+      sleep(250);
+      System.out.println("emitting : "+count);
+      emitter.next(count);
+    }
+
+    emitter.complete();
+  }
+
+  /**
+   *  TODO 07 Backpressure: subscriber in full control
+   *
+   *  A different strategy on backpressure is to have the subscriber ask for the next x elements when ready.
+   *  That's what we'll do here.
+   *
+   * Run the test and notice that nothing is printed, so let's see why.
+   *
+   *  As you can see below, we created our own Subscriber. The subscriber interface has 4 main methods :
+   *    onSubscribe()
+   *    onNext()
+   *    onError()
+   *    onComplete()
+   *
+   *  In the onNext() method we print the element we receive from the publisher, but apparently no element is emitted. That is because
+   *  the publisher assumes the worst, namely that the capacity of the subscriber is 0 (= subscriber can't handle any transmission).
+   *
+   *  Let's change this in the onSubscribe() method by requesting 1 element.
+   *  Run the test again and see how you now get 1 element transmitted (* 1)
+   *
+   *  What happened to the other elements? Well you still have to request for a new element when the previous element got processed.
+   *  Do this by updating the onNext() method, requesting 1 new element.
+   *
+   *  Run the test and now you should see all elements flowing through the pipeline.
+   *
+   *  Depending on what you want your initial capacity to be, you could change the onSubscribe() method to request 1 or more elements the first time.
+   *  Once you have processed 1 element you increase your capacity with 1, requesting the next element (in the onNext()), so you get a constant buffer.
+   *
+   *  To conclude: you as a subscriber now have full control over the number of elements you receive and you can buffer them if needed
+   */
+  @Test
+  public void backPressureFullControl() {
+    Flux<Integer> numbers = Flux.range(1, 10);
+
+    numbers.subscribe(new Subscriber<Integer>() {
+      private Subscription subscription;
+
+      @Override
+      public void onSubscribe(Subscription s) {
+        this.subscription = s;
+        subscription.request(1);
+      }
+
+      @Override
+      public void onNext(Integer integer) {
+        System.out.println("* "+integer);
+        subscription.request(1);
+      }
+
+      @Override
+      public void onError(Throwable t) {
+        System.out.println("ERROR ...");
+      }
+
+      @Override
+      public void onComplete() {
+        System.out.println("DONE ...");
+      }
+    });
+
+    sleep(2000);
+    System.out.println("**** END ****");
+  }
+
+  /**
+   * TODO 08 multiple subscribers
+   *
+   * In contrast to Java streams, a reactive stream can have multiple subscribers and there are different strategies on how you want to handle this.
+   * In the test below we have a publisher emitting data every second. Run it and see how it prints a new number every second.
+   *
+   * Make a second subscription with a delay of 5 seconds in between both subscriptions (HINT: use the sleep() method)
+   * Run your test and notice that the second subscriber jumps in after 5 seconds with the first element of the publisher : '0'
+   *
+   * This is like watching a movie on youtube where a second user wants to start from the beginning (= 2 different subscriptions)
+   *
+   * Now if we switch to television and the second user turns on his TV after 5 seconds, you would have missed the first 5 seconds and see what everyone
+   * else is seeing at that exact moment (= 1 shared subscription)
+   *
+   * Let's modify the exercise to do just that. We want to change the publisher to share the subscription. Look at the Flux API on how to do that.
+   * Run your test again and you should see the second subscription jumping in after 5 seconds getting the same data as the first subscriber
+   */
+  @Test
+  public void multipleSubscribers() {
+    Flux<Long> numbers = Flux.interval(Duration.ofSeconds(1)).share();
+
+    numbers.subscribe(process("subscriber 1:"));
+    sleep(5000);
+    numbers.subscribe(process("subscriber 2:"));
+
+    sleep(10000);
+  }
+
+  private Consumer<Long> process(String subscriber) {
+    return new Consumer<Long>() {
+      @Override
+      public void accept(Long l) {
+        System.out.println(subscriber + " " +l);
+      }
+    };
+  }
 }
